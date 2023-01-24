9a9642a49e46016b6ec8cc825c609db390d725c7
Eric Riera <ericrp2002@gmail.com>
2023-01-24 22:34:03 +0100

    White logo & cross in mobile nav

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
-const navToggle = document.querySelector("#toggle");
+const navToggle = document.querySelector(".toggle");
+const navCross = document.querySelector(".cross");
 const navMenu = document.querySelector(".menu");
 
 navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("menu_visible")
+    navMenu.classList.toggle("menu_visible");
+    navCross.classList.toggle("cross_visible");
+    navToggle.classList.toggle("toggle_visible");
 });
 
 navMenu.addEventListener("click", () => {
-    navMenu.classList.toggle("menu_visible")
+    navMenu.classList.toggle("menu_visible");
+    navCross.classList.toggle("cross_visible");
+    navToggle.classList.toggle("toggle_visible");
+});
+
+navCross.addEventListener("click", () => {
+    navMenu.classList.toggle("menu_visible");
+    navCross.classList.toggle("cross_visible");
+    navToggle.classList.toggle("toggle_visible");
 });

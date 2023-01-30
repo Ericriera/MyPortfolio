c09822ac330471bf0ebdeb2aa998d5820d1e90f9
Eric Riera <ericrp2002@gmail.com>
2023-01-31 00:56:39 +0100

    Actualized about me & fixed bugs

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 function close_nav() {
     openmenu = false;
-    navMenu.classList.remove("menu_visible");
+    navMenu.style.left = '100%';
+    navMenu.style.backgroundColor = 'transparent';
     navCross.classList.remove("cross_visible");
     navToggle.classList.add("toggle_visible");
-    if(getPageScroll().top > 20 && darkmode) nav.style.background = "#222";
-    else if (getPageScroll().top > 20) nav.style.background = "#364552";
-    else nav.style.background = "transparent";
+    if(getPageScroll().top > 20 && darkmode) nav.style.background = '#222';
+    else if (getPageScroll().top > 20) nav.style.background = '#364552';
+    else nav.style.background = 'transparent';
 }
 
 function getPageScroll() {
@@ -40,12 +41,11 @@ window.addEventListener("scroll", () => {
         logo.style.padding = "0 2rem";
     }
     else {
-        if(!openmenu) {
-            nav.style.background = "transparent";
-            if(document.documentElement.clientWidth >= 540) navMenu.style.background = "transparent";
+        if(!openmenu) nav.style.background = "transparent";
+        if(document.documentElement.clientWidth >= 540) {
+            navMenu.style.lineHeight = "90px";
+            logo.style.padding = "10px 2rem";
         }
-        navMenu.style.lineHeight = "90px";
-        if(document.documentElement.clientWidth >= 510) logo.style.padding = "10px 2rem";
     }
 })
 
@@ -53,22 +53,29 @@ window.addEventListener("scroll", () => {
 const navToggle = document.querySelector(".toggle");
 const navCross = document.querySelector(".cross");
 const projects = document.getElementById("nProjects");
-const aboutme = document.getElementById("nAboutme");
+const navAboutme = document.getElementById("nAboutme");
 
 navToggle.addEventListener("click", () => {
     openmenu = true;
-    navMenu.classList.add("menu_visible");
+    navMenu.style.left = '0';
+    if (darkmode) {
+        nav.style.backgroundColor = "#222";
+        navMenu.style.backgroundColor = "#222";
+    }
+    else {
+        nav.style.backgroundColor = "#364552";
+        navMenu.style.backgroundColor = "#364552";
+    } 
     navCross.classList.add("cross_visible");
-    navToggle.classList.remove("toggle_visible"); 
-    if (darkmode) nav.style.background = "#222";
-    else nav.style.background = "#364552";
+    navToggle.classList.remove("toggle_visible");
+    
 });
 
 projects.addEventListener("click", () => {
     close_nav();
 });
 
-aboutme.addEventListener("click", () => {
+navAboutme.addEventListener("click", () => {
     close_nav();
 });
 
@@ -78,8 +85,12 @@ navCross.addEventListener("click", () => {
 
 // Change Language
 const lang = document.getElementById('lang');
-const esab = 'Mi nombre es Eric Riera y actualmente estoy en el tercer año de la carrera de Ingenieria Informatica cursando la especialidad de ingenieria del software en FIB-UPC.';
-const enab = 'My name is Eric Riera and I am currently in my third year of Computer Engineering degree in software engineering at FIB-UPC.';
+const esab1 = 'Mi nombre es Eric Riera y actualmente estoy en el tercer año de la carrera de Ingenieria Informatica cursando la especialidad de ingenieria del software en FIB-UPC.';
+const esab2 = 'Me describo como una persona que aprende rápido y con buena capacidad de solucionar problemas. Me gusta aprender cosas nuevas cada día y asumir nuevos retos.';
+const esab3 = 'Cuando tengo tiempo libre, algunos de mis hobbies son jugar a baloncesto, ir al gimnasio, trabajar en proyectos personales, ver pelis o series y escuchar música. También me gusta viajar y pasar tiempo con mi familia y amigos.';
+const enab1 = 'My name is Eric Riera and I am currently in my third year of Computer Engineering degree in software engineering at FIB-UPC.';
+const enab2 = 'I describe myself as a fast learner and a good problem solver. I like learning new things every day and taking on new challenges.';
+const enab3 = 'When I have free time, some of my hobbies are playing basketball, going to the gym, working on personal projects, watching series or movies and listening to music. I also love to travel and spend time with my family and friends.';
 
 lang.addEventListener("click", () => {
     if (en) {
@@ -94,7 +105,9 @@ lang.addEventListener("click", () => {
         document.getElementById('tPortfolio').innerText = 'Mi Portfolio';
         document.getElementById('tPROP').innerText = 'Gestor de Archivos';
         document.getElementById('tAboutme').innerText = 'Sobre mi';
-        document.getElementById('txtAM').innerText = esab;
+        document.getElementById('txtAM1').innerText = esab1;
+        document.getElementById('txtAM2').innerText = esab2;
+        document.getElementById('txtAM3').innerText = esab3;
         document.getElementById('fBTT').innerText = 'Volver Arriba ↑';
         document.getElementById('fContactme').innerText = 'Contáctame';
         document.getElementById('fResume').innerText = 'CV';
@@ -112,7 +125,9 @@ lang.addEventListener("click", () => {
         document.getElementById('tPortfolio').innerText = 'My Portfolio';
         document.getElementById('tPROP').innerText = 'File Manager';
         document.getElementById('tAboutme').innerText = 'About me';
-        document.getElementById('txtAM').innerText = enab;
+        document.getElementById('txtAM1').innerText = enab1;
+        document.getElementById('txtAM2').innerText = enab2;
+        document.getElementById('txtAM3').innerText = enab3;
         document.getElementById('fBTT').innerText = 'Back to top ↑';
         document.getElementById('fContactme').innerText = 'Contact me';
         document.getElementById('fResume').innerText = 'Resume';
@@ -125,32 +140,34 @@ const body = document.querySelector("body");
 const targeta = document.querySelectorAll(".targeta");
 const sun = document.getElementById('sun');
 const moon = document.getElementById('moon');
+const aboutme = document.querySelector(".txt");
 
 sun.addEventListener("click", () => {
     darkmode = false;
-    //close_nav();
     body.classList.add("body");
+    aboutme.classList.add("light");
     targeta.forEach(element => {
-        element.classList.remove("darktargeta");
+        element.classList.add("light");
     });
+    backToTopButton.style.color = 'white';
     sun.style.display = "none";
-    if(document.documentElement.clientWidth < 510) moon.style.display = "block";
+    if(document.documentElement.clientWidth < 540) moon.style.display = "block";
     else moon.style.display = "inline";
     if(getPageScroll().top > 20 || openmenu) {
         nav.style.background = "#364552";
         navMenu.style.background = "#364552";
-    } 
+    }
 });
 
 moon.addEventListener("click", () => {
     darkmode = true;
-    //close_nav();
     body.classList.remove("body");
+    aboutme.classList.remove("light");
     targeta.forEach(element => {
-        element.classList.add("darktargeta");
+        element.classList.remove("light");
     });
-    if(getPageScroll().top > 20) nav.style.background = "#222";
-    if(document.documentElement.clientWidth < 510) sun.style.display = "block";
+    backToTopButton.style.color = 'black';
+    if(document.documentElement.clientWidth < 540) sun.style.display = "block";
     else sun.style.display = "inline";
     moon.style.display = "none";
     if(getPageScroll().top > 20 || openmenu) {
@@ -159,22 +176,22 @@ moon.addEventListener("click", () => {
     }
 });
 
-// Sun & moon display actualization for resize
+// Display actualization for resize
 window.addEventListener("resize", () => {
     close_nav();
-    if (document.documentElement.clientWidth < 540) {
+    if(document.documentElement.clientWidth < 540 || getPageScroll.top > 20) {
+        navToggle.classList.add("toggle_visible");
+        navMenu.style.lineHeight = "70px";
+        logo.style.padding = "0px 2rem";
         if(sun.style.display == "inline") sun.style.display = "block";
         if(moon.style.display == "inline") moon.style.display = "block";
-        if(darkmode) navMenu.style.background = "#222";
-        else navMenu.style.background = "#364552";
     }
-    
-    if(document.documentElement.clientWidth >= 540) {
+    else {
+        navMenu.style.lineHeight = "90px";
+        logo.style.padding = "10px 2rem";
         if(sun.style.display == "block") sun.style.display = "inline";
         if(moon.style.display == "block") moon.style.display = "inline";
-        navMenu.style.background = "transparent";
-    }
-    
+    }   
 });
 
 //TypeWritter subtitle

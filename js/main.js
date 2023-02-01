7cff303e31f39f3ad1bc41d87f8a12dd8bfd9fa3
Eric Riera <ericrp2002@gmail.com>
2023-02-01 16:03:20 +0100

    Scrollbar modified

--- js/main.js
+++ js/main.js
@@ -244,3 +244,18 @@ window.addEventListener("scroll", () => {
 backToTopButton.addEventListener("click", () => {
     document.body.scrollIntoView();
 });
+
+window.addEventListener('scroll', function()  {
+    let elements = document.getElementsByClassName('scroll-content');
+    let screenSize = window.innerHeight;
+    
+      for(var i = 0; i < elements.length; i++) {
+        let element = elements[i];
+  
+        if(element.getBoundingClientRect().top < screenSize) {
+          element.classList.add('visible');
+        } else {
+          element.classList.remove('visible');
+        }
+      }
+  });
